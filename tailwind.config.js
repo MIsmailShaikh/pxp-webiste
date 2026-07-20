7978fd729bca8f12e9d1c76b519e09bf7668f821
MIsmailShaikh <MIsmailShaikh@users.noreply.github.com>
2026-07-20 16:29:11 +0530

    Optimize CSS loading: removed Tailwind CDN, added compilation build step

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        pxp: {
+          blue: '#1A365D',
+          lightBlue: '#2B6CB0',
+          accent: '#3182CE'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
